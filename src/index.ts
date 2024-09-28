4b0f3748f4eb4d0b13c282a5d94d27b8edb347e6
Aleksej Domovec <45975492+aleksejdom@users.noreply.github.com>
2024-09-28 23:22:21 +0200

    3d head

--- src/index.ts
+++ src/index.ts
@@ -1,77 +1,225 @@
 import {
-    ViewerApp,
-    AssetManagerPlugin,
-    GBufferPlugin,
-    timeout,
-    ProgressivePlugin,
-    TonemapPlugin,
-    SSRPlugin,
-    SSAOPlugin,
-    DiamondPlugin,
-    FrameFadePlugin,
-    GLTFAnimationPlugin,
-    GroundPlugin,
-    BloomPlugin,
-    TemporalAAPlugin,
-    AnisotropyPlugin,
-    GammaCorrectionPlugin,
-
-    addBasePlugins,
-    ITexture, TweakpaneUiPlugin, AssetManagerBasicPopupPlugin, CanvasSnipperPlugin,
-
-    IViewerPlugin, FileTransferPlugin,
-
-    // Color, // Import THREE.js internals
-    // Texture, // Import THREE.js internals
+  ViewerApp,
+  AssetManagerPlugin,
+  GBufferPlugin,
+  timeout,
+  ProgressivePlugin,
+  TonemapPlugin,
+  SSRPlugin,
+  SSAOPlugin,
+  DiamondPlugin,
+  FrameFadePlugin,
+  GLTFAnimationPlugin,
+  GroundPlugin,
+  BloomPlugin,
+  TemporalAAPlugin,
+  AnisotropyPlugin,
+  GammaCorrectionPlugin,
+  addBasePlugins,
+  ITexture,
+  TweakpaneUiPlugin,
+  AssetManagerBasicPopupPlugin,
+  CanvasSnipperPlugin,
+  IViewerPlugin,
+  FileTransferPlugin,
+  FullScreenPlugin,
+  AssetImporter,
+  mobileAndTabletCheck,
 } from "webgi";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Lenis from "@studio-freight/lenis";
+
+gsap.registerPlugin(ScrollTrigger);
+
 import "./styles.css";
 
-async function setupViewer(){
+async function setupViewer() {
+  // Initialize the viewer
+  const viewer = new ViewerApp({
+    canvas: document.getElementById("webgi-canvas") as HTMLCanvasElement,
+    useRgbm: false,
+  });
+  
+  const manager = await viewer.addPlugin(AssetManagerPlugin);
+  const camera = viewer.scene.activeCamera;
+  const position = camera.position;
+  const target = camera.target;
+  const isMobile = mobileAndTabletCheck()
 
-    // Initialize the viewer
-    const viewer = new ViewerApp({
-        canvas: document.getElementById('webgi-canvas') as HTMLCanvasElement,
+  await viewer.addPlugin(GBufferPlugin);
+  await viewer.addPlugin(SSAOPlugin);
+  await viewer.addPlugin(BloomPlugin);
+  await viewer.addPlugin(AnisotropyPlugin);
+  await viewer.getOrAddPlugin(GammaCorrectionPlugin)
+  await viewer.getOrAddPlugin(FullScreenPlugin)
+  await addBasePlugins(viewer);
+  // check the source: https://codepen.io/repalash/pen/JjLxGmy for the list of plugins added.
+
+  //Loader
+  const importer = manager.importer as AssetImporter;
+  importer.addEventListener("onProgress", (e)=>{
+    const progressRatio = (e.loaded);
+    document.querySelector('.progress')?.setAttribute('style', `transform: scaleX(${progressRatio})`)
+  })
+  importer.addEventListener("onLoad", (e)=>{
+    gsap.to('.loader', {
+      x: "100%",
+      duration:0.8,
+      ease: "power4.inOut",
+      delay: 1,
+      onComplete: () => {
+        document.body.style.overflowY = 'atuo';
+      }
+    })
+  })
+
+  await viewer.addPlugin(AssetManagerBasicPopupPlugin);
+  await viewer.addPlugin(FileTransferPlugin);
+  await viewer.addPlugin(CanvasSnipperPlugin);
+
+  await viewer.load("./assets/head.glb");
+  
+  viewer.renderer.refreshPipeline()
+  viewer.scene.activeCamera.setCameraOptions({controlsEnabled: false})
+
+  if(isMobile) {
+    position.set(-2.19, -3.71, 16.93)
+    target.set(0.13,0.08,-0.18)
+    camera.setCameraOptions({fov:25})
+  }
+  
+  // Initialize Lenis for smooth scrolling
+  const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    wheelMultiplier: 1,
+  });
+
+  function raf(time: any) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+  }
+  requestAnimationFrame(raf);
+
+  lenis.on("scroll", (e: any) => {
+    /* console.log(e); */
+  });
+
+  // WEBGL UPDATE
+  let needsUpdate = true;
+
+  function onUpdate() {
+    needsUpdate = true;
+    viewer.renderer.resetShadows();
+  }
+
+  function setupScrollAnimation() {
+    const tl = gsap.timeline();
+
+    // First section animation
+    gsap.set(position, {
+      x: -2.74,
+      y: 0.68,
+      z: 13.5
+    }) 
+    gsap.set(target, {
+      x: -0.20,
+      y: -0.06,
+      z: -0.06
     })
 
-    // Add plugins individually.
-    // await viewer.addPlugin(GBufferPlugin)
-    // await viewer.addPlugin(new ProgressivePlugin(32))
-    // await viewer.addPlugin(new TonemapPlugin(!viewer.useRgbm))
-    // await viewer.addPlugin(GammaCorrectionPlugin)
-    // await viewer.addPlugin(SSRPlugin)
-    // await viewer.addPlugin(SSAOPlugin)
-    // await viewer.addPlugin(DiamondPlugin)
-    // await viewer.addPlugin(FrameFadePlugin)
-    // await viewer.addPlugin(GLTFAnimationPlugin)
-    // await viewer.addPlugin(GroundPlugin)
-    // await viewer.addPlugin(BloomPlugin)
-    // await viewer.addPlugin(TemporalAAPlugin)
-    // await viewer.addPlugin(AnisotropyPlugin)
-    // and many more...
+    // Second section animation 
+    tl.to(position, {
+      x: -4.96,
+      y: 1.60,
+      z: 5.78,
+      duration: 4,
+      scrollTrigger: {
+        trigger: ".section--2",
+        start: "top bottom",
+        end: "top top",
+        scrub: true,
+        onUpdate,
+      },
+    })
+    .to('.section--1 > .text--wrap', {
+      xPercent: '-100',
+      opacity: 0,
+      duration: 4,
+      scrollTrigger: {
+        trigger: ".section--2",
+        start: "top bottom",
+        end: "top top",
+        scrub: true, 
+        onUpdate,
+      },
+    })
+    .to(target, {
+      x: 0.17,
+      y: 0.47,
+      z: 0.98,
+      duration: 4,
+      scrollTrigger: {
+        trigger: ".section--2",
+        start: "top bottom",
+        end: "top top",
+        scrub: true,
+        onUpdate,
+      },
+    })
 
-    // or use this to add all main ones at once.
-    await addBasePlugins(viewer) // check the source: https://codepen.io/repalash/pen/JjLxGmy for the list of plugins added.
+    // CONTACT section animation
+    .to(position, {
+      x: 1.28,
+      y: 0.57,
+      z: 5.86,
+      duration: 6,
+      scrollTrigger: {
+        trigger: ".section--contact",
+        start: "top bottom",
+        end: "top top",
+        scrub: true,   
+        onUpdate,
+      },
+    })
+    .to('.section--2 > .text--wrap', {
+      xPercent: '100',
+      opacity: 0,
+      duration: 4,
+      scrollTrigger: {
+        trigger: ".section--contact",
+        start: "top bottom",
+        end: "top top",
+        scrub: true, 
+        onUpdate,
+      },
+    })
+    .to(target, {
+      x: -0.64,
+      y: 0.44,
+      z: 0.73,
+      duration: 6,
+      scrollTrigger: {
+        trigger: ".section--contact",
+        start: "top bottom",
+        end: "top top",
+        scrub: true,  
+        onUpdate,
+      },
+    });
+  }
 
-    // Add a popup(in HTML) with download progress when any asset is downloading.
-    await viewer.addPlugin(AssetManagerBasicPopupPlugin)
+  onUpdate();
+  setupScrollAnimation();
 
-    // Required for downloading files from the UI
-    await viewer.addPlugin(FileTransferPlugin)
-
-    // Add more plugins not available in base, like CanvasSnipperPlugin which has helpers to download an image of the canvas.
-    await viewer.addPlugin(CanvasSnipperPlugin)
-
-    // Import and add a GLB file.
-    await viewer.load("./assets/classic-watch.glb")
-
-    // Load an environment map if not set in the glb file
-    // await viewer.setEnvironmentMap("./assets/environment.hdr");
-
-    // Add some UI for tweak and testing.
-    const uiPlugin = await viewer.addPlugin(TweakpaneUiPlugin)
-    // Add plugins to the UI to see their settings.
-    uiPlugin.setupPlugins<IViewerPlugin>(TonemapPlugin, CanvasSnipperPlugin)
-
+  viewer.addEventListener("preFrame", () => {
+    if (needsUpdate) {
+      camera.positionUpdated(false);
+      camera.targetUpdated(true);
+      needsUpdate = false;
+    }
+  });
 }
 
-setupViewer()
+setupViewer();
